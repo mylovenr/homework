d0887361eb65c9cf954e68108cb4f929dd5e8ba3
mylovenr <yangdayoung11@hanmail.net>
2014-08-22 17:55:41 +0900

    no message

---
+++ last_week/likelion_flaskr_login/apps/settings.py
@@ -0,0 +1,24 @@
+"""
+settings.py
+
+Configuration for Flask app
+
+"""
+import os
+from datetime import timedelta
+
+
+class Config(object):
+    # Set secret key to use session
+    SECRET_KEY = "likelion-flaskr-secret-key"
+    debug = False
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=2)
+
+
+class Production(Config):
+    debug = True
+    CSRF_ENABLED = False
+    ADMIN = "day4745@gmail.com"
+    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///blog?instance=dayandtoday:dayandtoday-db'
+    migration_directory = 'migrations'
+
